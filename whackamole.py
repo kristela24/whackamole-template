88af1f65ca70b06b974cdf0fbad050a6654ae001
unknown <malaj.kristela@gmail.com>
2024-11-18 21:26:18 -0500

    comment

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,6 @@
 import pygame
-
+import random
+from constants import *
 
 def main():
     try:
@@ -10,11 +11,33 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+
+        #mole start
+        mole_x = 0
+        mole_y = 0
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                #mole-mouse movement
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = event.pos
+                    mole_rect = mole_image.get_rect(topleft=(mole_x, mole_y))
+
+                    if mole_rect.collidepoint(mouse_x, mouse_y):
+                        # Move mole to random square
+                        mole_x = random.randrange(0, 20) * 32
+                        mole_y = random.randrange(0, 16) * 32
+
+        #screen
+            screen.fill("light blue")
+            for x in range(0, 641, 32):  # 640 + 1 to include right edge
+                pygame.draw.line(screen, "dark green", (x, 0), (x, 512))
+            for y in range(0, 513, 32):  # 512 + 1 to include bottom edge
+                pygame.draw.line(screen, "white", (0, y), (640, y))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mole_x, mole_y)))
+
             pygame.display.flip()
             clock.tick(60)
     finally:
@@ -23,4 +46,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-#heehlhoorhh
+#update
